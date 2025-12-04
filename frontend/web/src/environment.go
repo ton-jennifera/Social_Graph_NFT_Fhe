240244f29b5be0e1cf72364d1f10147345a07257
ton-jennifera <ChrisKudrle1144@outlook.com>
2025-12-05 07:31:15 +0800

    add order.jsx, environment.go, index.ts

---
+++ frontend/web/src/environment.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\environment.go
+// Generated at: 2025-10-20T20:11:12.839Z
---
+++ frontend/web/src/order.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+interface account-adminProps { data: string }
